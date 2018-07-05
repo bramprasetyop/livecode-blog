312ff438cd7c20599cd299d80196fbda80bd0cb5
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-07-05 17:25:18 +0700

    register-button-done

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -3,25 +3,69 @@
     <div class="row">
       <div id="registering" class="col s6 right">
         <div id="daftardah" class="col s4 right">
-          <a>
-            <h6>Daftar</h6>
+          <a onclick="document.getElementById('id03').style.display='block'">
+            <h6 id="signupp">Signup</h6>
           </a>
         </div>
       </div>
     </div>
 
     <h2 id="heades">Go-Blog</h2>
+
+    <div id="id03" class="modal">
+      <form class="modal-content animate" action="/action_page.php">
+        <div class="imgcontainer">
+          <span onclick="document.getElementById('id03').style.display='none'" class="close">&times;</span>
+        </div>
+        <div class="container">
+          <label for="name">
+            <b>Username</b>
+          </label>
+          <input v-model="usernamesignup" type="text" placeholder="Username..." name="name">
+
+          <label for="psw">
+            <b>Password</b>
+          </label>
+          <input v-model="passwordsignup" type="password" placeholder="Password..." name="psw">
+
+          <button onclick="document.getElementById('id03').style.display='none'" @click="signup()" type="button">Signup</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      usernamesignup: '',
+      passwordsignup: ''
+    }
   },
   methods: {
-    signup(){
+    signup() {
+      let obj = {
+        username: this.usernamesignup,
+        password: this.passwordsignup
+      }
+      axios
+        .post('http://localhost:3000', obj)
+        .then(response => {
+          // console.log(response)
 
+          this.$router.push('/')
+          swal({
+            text: 'Signup Success',
+            icon: 'success'
+          })
+        })
+        .catch(err => {
+          swal({
+            text: 'Something Wrong',
+            icon: 'error'
+          })
+        })
     }
   }
 }
@@ -31,6 +75,9 @@ export default {
   margin-right: 15px;
 }
 
+#signupp{
+  color: whitesmoke;
+}
 .ininavbar {
   /* border: 1px solid black; */
   width: 100%;
@@ -52,7 +99,7 @@ export default {
   margin-top: 10px;
   /* border: 1px solid rgb(5, 37, 36); */
   border-radius: 10px;
-  background-color: rgb(39, 189, 164)
+  background-color: rgb(39, 189, 164);
 }
 </style>
 
