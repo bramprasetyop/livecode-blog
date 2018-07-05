935fa5ee079af9ae298de4a34e581525bb13f11e
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-07-05 17:14:48 +0700

    register-member

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -1,8 +1,18 @@
 <template>
-<div class="ininavbar">
+  <div class="ininavbar">
+    <div class="row">
+      <div id="registering" class="col s6 right">
+        <div id="daftardah" class="col s4 right">
+          <a>
+            <h6>Daftar</h6>
+          </a>
+        </div>
+      </div>
+    </div>
 
-  <h2 id="heades">Go-Blog</h2>
-</div></template>
+    <h2 id="heades">Go-Blog</h2>
+  </div>
+</template>
 
 <script>
 export default {
@@ -10,34 +20,40 @@ export default {
     return {}
   },
   methods: {
-    logout() {
-      localStorage.clear()
-      this.$router.push('/')
+    signup(){
+
     }
   }
 }
 </script>
 <style>
-
 #logout {
   margin-right: 15px;
 }
 
-.ininavbar{
+.ininavbar {
   /* border: 1px solid black; */
   width: 100%;
   height: 150px;
-  margin-top: -95px;
-  background-color: rgb(172, 194, 194)
+  margin-top: -60px;
+  background-color: rgb(172, 194, 194);
 }
 
-#heades{
+#heades {
   font-family: sans-serif;
 }
 
+#registering {
+  /* border: 1px solid black; */
+}
 
-
-
+#daftardah {
+  margin-right: 0px;
+  margin-top: 10px;
+  /* border: 1px solid rgb(5, 37, 36); */
+  border-radius: 10px;
+  background-color: rgb(39, 189, 164)
+}
 </style>
 
 
